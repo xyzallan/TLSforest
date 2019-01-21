214a578dfd489cd8291481f089b0abe951fc6840
Allan Sims <allan@metsateave.ee>
2019-01-21 20:31:31 +0200

    Update neighbCount.R

--- R/neighbCount.R
+++ R/neighbCount.R
@@ -1,3 +1,5 @@
+# Neighbours count from 2D data
+
 neighbCount2d <- function(x, y, radius = 0.05)
 {
   cnc <- function(x, rd)
@@ -8,6 +10,7 @@ neighbCount2d <- function(x, y, radius = 0.05)
   apply(mtr, 1, cnc, radius)
 }
 
+# Neighbours count from 3D data
 neighbCount3d <- function(x, y, z, radius = 0.05)
 {
   cnc <- function(x, rd)
