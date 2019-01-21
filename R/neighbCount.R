05c502ef1d312666fa73029c4530eab41036cfda
Allan Sims <allan@metsateave.ee>
2019-01-21 20:33:20 +0200

    Update neighbCount.R

--- R/neighbCount.R
+++ R/neighbCount.R
@@ -6,7 +6,9 @@ neighbCount2d <- function(x, y, radius = 0.05)
   {
     length(x[x <= rd])
   }
-  apply(distMatr2d(x, y), 1, cnc, radius)
+  apply(
+    distMatr2d(x, y)
+    , 1, cnc, radius)
 }
 
 # Neighbours count from 3D data
@@ -16,5 +18,7 @@ neighbCount3d <- function(x, y, z, radius = 0.05)
   {
     length(x[x <= rd])
   }
-  apply(distMatr3d(x, y, z), 1, cnc, radius)
+  apply(
+    distMatr3d(x, y, z)
+    , 1, cnc, radius)
 }
