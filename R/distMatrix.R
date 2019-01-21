485f842796ca506f54f9532d2d306100ac93f31f
Allan Sims <allan@metsateave.ee>
2019-01-21 20:30:38 +0200

    Update distMatrix.R

--- R/distMatrix.R
+++ R/distMatrix.R
@@ -1,4 +1,12 @@
+# Matrix of distances from 2D data
 distMatr2d <- function(x, y)
 {
   sqrt(outer(x, x,"-")^2 + outer(y, y,"-")^2)
 }
+
+# Matrix of distances from 3D data
+distMatr3d <- function(x, y, z)
+{
+  sqrt(outer(x, x,"-")^2 + outer(y, y,"-")^2 + outer(z, z,"-")^2)
+}
+
